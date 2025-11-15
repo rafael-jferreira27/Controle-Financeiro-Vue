75efd4600844d0ed1cc987a22d15a08644dcf175
Rafaél José <rafael.jferreira27@gmail.com>
2025-11-15 01:55:22 -0300

    Adiciona declarações para arquivos .vue

---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+// src/env.d.ts
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
